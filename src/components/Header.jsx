7c3578d32f8fba70df7b0f658732c7a3ad764967
adhit7 <adhithyanalan@gmail.com>
2024-02-29 16:36:41 +0530

    removed unused keywords

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Container } from 'react-bootstrap';
 
 const Header = () => {
   return (
